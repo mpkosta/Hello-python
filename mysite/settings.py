de3fbd825a0b96f185c153370ad4e4e1021f2b65
mpkosta <mpkosta@protonmail.com>
2026-01-14 23:12:30 +0200

    Update settings to use Postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'linux_project',       # The name you used in the CREATE DATABASE command
+        'USER': 'mkosta',             # The user you created
+        'PASSWORD': 'admin',     # The password you set
+        'HOST': 'localhost',          # Localhost means "this computer"
+        'PORT': '',                   # Leave empty to use the default port
     }
 }
 
